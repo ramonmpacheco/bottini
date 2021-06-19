489ee4c0c5bc369d6b964bf53a9c1f91d5a7f743
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-19 07:11:35 -0300

    refact: product grid

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -1,29 +1,14 @@
-import 'package:bottini/providers/products.dart';
-import 'package:bottini/widgets/product_item.dart';
+import 'package:bottini/widgets/product_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.items;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Loja Bottini'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: products.length,
-        itemBuilder: (ctx, index) => ProductItem(products[index]),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
