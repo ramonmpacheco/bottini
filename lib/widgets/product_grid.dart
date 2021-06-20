6a5d0e278c3c28e90e54d57792b15b640b4ae888
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-20 07:43:38 -0300

    fix: ChangeNotifierProvider

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -12,8 +12,8 @@ class ProductGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        create: (_) => products[index],
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
         child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
