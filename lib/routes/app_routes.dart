713d5f65824c6f0d823638b01529e9af000fa79b
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-24 06:10:24 -0300

    add: Orders Screen

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,6 @@
 class AppRoutes {
+  static const HOME = '/home';
+  static const ORDERS = '/orders';
   static const CART_DETAIL = '/cart-detail';
   static const PRODUCT_DETAIL = '/product-detail';
 }
