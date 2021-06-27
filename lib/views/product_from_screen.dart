f98e7283e9059eb6575c8644fcb8c957deaf87e9
Ramon Pacheco <rmpacheco2@gmail.com>
2021-06-27 07:59:52 -0300

    add: product form inputs

--- lib/views/product_from_screen.dart
+++ lib/views/product_from_screen.dart
@@ -9,12 +9,26 @@ class ProductFormScreem extends StatefulWidget {
 class _ProductFormScreemState extends State<ProductFormScreem> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
 
   @override
   void dispose() {
     super.dispose();
     _priceFocusNode.dispose();
     _descriptionFocusNode.dispose();
+    _imageUrlFocusNode.removeListener(_updateImage);
+    _imageUrlFocusNode.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocusNode.addListener(_updateImage);
+  }
+
+  void _updateImage() {
+    setState(() {});
   }
 
   @override
@@ -37,10 +51,10 @@ class _ProductFormScreemState extends State<ProductFormScreem> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Preço'),
                 textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) =>
-                    FocusScope.of(context).requestFocus(_descriptionFocusNode),
                 focusNode: _priceFocusNode,
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_descriptionFocusNode),
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Descrição'),
@@ -48,6 +62,40 @@ class _ProductFormScreemState extends State<ProductFormScreem> {
                 keyboardType: TextInputType.multiline,
                 textInputAction: TextInputAction.next,
                 focusNode: _descriptionFocusNode,
+                onFieldSubmitted: (_) =>
+                    FocusScope.of(context).requestFocus(_imageUrlFocusNode),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Url da Imagem'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _imageUrlFocusNode,
+                      controller: _imageUrlController,
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 8, left: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1,
+                      ),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Informe a URL')
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ],
               ),
             ],
           ),
