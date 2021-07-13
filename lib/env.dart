b8ddece40e6ae9904837b443b79b102a0c6a7e11
Ramon Pacheco <rmpacheco2@gmail.com>
2021-07-13 19:18:05 -0300

    add: save order via rest

--- lib/env.dart
+++ lib/env.dart
@@ -1,3 +1,4 @@
 class AppRoutes {
   static const PRODUCT_BASE_URL = 'https://minha_url/products';
+  static const ORDERS_BASE_URL = 'https://minha_url/orders';
 }
--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
+import 'package:bottini/env.dart';
 import 'package:bottini/providers/cart.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
 
 class Order {
   final String id;
@@ -18,6 +21,8 @@ class Order {
 }
 
 class Orders with ChangeNotifier {
+  final String _baseUrl = "${AppRoutes.ORDERS_BASE_URL}";
+
   List<Order> _items = [];
 
   List<Order> get items {
@@ -28,13 +33,30 @@ class Orders with ChangeNotifier {
     return _items.length;
   }
 
-  void addOrder(Cart cart) {
+  Future<void> addOrder(Cart cart) async {
+    final date = DateTime.now();
+    final response = await http.post(
+      Uri.parse("$_baseUrl.json"),
+      body: json.encode({
+        "total": cart.totalAmount,
+        "date": date.toIso8601String(),
+        "products": cart.items.values
+            .map((cartItem) => {
+                  "id": cartItem.id,
+                  "productId": cartItem.productId,
+                  "title": cartItem.title,
+                  "quantity": cartItem.quantity,
+                  "price": cartItem.price,
+                })
+            .toList()
+      }),
+    );
     _items.insert(
       0,
       Order(
-        id: Random().nextDouble().toString(),
+        id: json.decode(response.body)['name'],
         total: cart.totalAmount,
-        date: DateTime.now(),
+        date: date,
         products: cart.items.values.toList(),
       ),
     );
