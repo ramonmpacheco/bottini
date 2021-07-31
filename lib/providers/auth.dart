35453e2bf6d35e4b81ac8823e7002936930d34c4
Ramon Pacheco <rmpacheco2@gmail.com>
2021-07-31 17:40:36 -0300

    add: auto login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:bottini/data/local_store.dart';
 import 'package:bottini/env.dart';
 import 'package:bottini/exceptions/auth_exception.dart';
 import 'package:flutter/cupertino.dart';
@@ -54,6 +55,13 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseBody['expiresIn']),
         ),
       );
+
+      LocalStore.saveMap('userData', {
+        "token": _token,
+        "userId": _userId,
+        "expireDate": _expireDate.toIso8601String(),
+      });
+
       _autoLogout();
       notifyListeners();
     }
@@ -71,6 +79,31 @@ class Auth with ChangeNotifier {
     return this._authenticate(email, password, url);
   }
 
+  Future<void> tryAutoLogin() async {
+    if (isAuthenticated) {
+      return Future.value();
+    }
+
+    final userData = await LocalStore.getMap('userData');
+    if (userData == null) {
+      return Future.value();
+    }
+
+    final expireDate = DateTime.parse(userData['expireDate']);
+    if (expireDate.isBefore(DateTime.now())) {
+      return Future.value();
+    }
+
+    this._token = userData['token'];
+    this._userId = userData['userId'];
+    this._expireDate = expireDate;
+
+    _autoLogout();
+    notifyListeners();
+
+    return Future.value();
+  }
+
   void logout() {
     _token = null;
     _userId = null;
@@ -79,6 +112,7 @@ class Auth with ChangeNotifier {
       _logoutTimer.cancel();
       _logoutTimer = null;
     }
+    LocalStore.remove('userData');
     notifyListeners();
   }
 
